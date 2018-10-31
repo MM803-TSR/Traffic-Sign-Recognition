dd5ae68fb5ec61ff8848fc3b892be99941c658dc
IAMDXX <yiting5@ualberta.ca>
2018-10-31 15:34:21 -0600

    Update video_to_img.py

--- TSR/preprocessing/video_to_img.py
+++ TSR/preprocessing/video_to_img.py
@@ -26,10 +26,12 @@ for v in Videos:
         # Capture frame-by-frame
         ret, frame = cap.read()
         if ret == True:
-            # Display the resulting frame
-            #cv2.imshow('Frame',frame)
-            cv2.imwrite("frame %d.jpg" % count,frame)
-            count +=1 
+            count +=1
+            if count %15 == 0:
+                # Display the resulting frame
+                #cv2.imshow('Frame',frame)
+                cv2.imwrite("frame %d.jpg" % count,frame)
+             
         # Press Q on keyboard to  exit
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
