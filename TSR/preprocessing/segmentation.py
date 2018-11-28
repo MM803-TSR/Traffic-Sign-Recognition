2d79174dbd080493139e1a8897111b1bb3f01469
luchen <luchenliu929@gmail.com>
2018-11-27 22:51:27 -0700

    Combined all steps within this file, the original version of code is copied as 'segmentation1.py'. The result is good up to now. To-do: Add if..else for shape confidence.

--- TSR/preprocessing/segmentation.py
+++ TSR/preprocessing/segmentation.py
@@ -4,124 +4,124 @@ import numpy as np
 
 
 def load_prep_img(img_path):
-	sample_img = cv2.imread(img_path)
-	size = sample_img.shape
-	blank_canvas = np.zeros(size, dtype=np.uint8)
-	# Convert to RGB color space
-	rgb_img = cv2.cvtColor(sample_img, cv2.COLOR_BGR2RGB)
-	# Convert to HSV color space
-	hsv_img = cv2.cvtColor(sample_img, cv2.COLOR_BGR2HSV)
-	# Blur image to reduce unrelated noise
-	blur_img = cv2.bilateralFilter(hsv_img, 9, 75, 75)
-	return blank_canvas, sample_img, rgb_img, hsv_img, blur_img
+    sample_img = cv2.imread(img_path)
+    size = sample_img.shape
+    blank_canvas = np.zeros(size, dtype=np.uint8)
+    # Convert to RGB color space
+    rgb_img = cv2.cvtColor(sample_img, cv2.COLOR_BGR2RGB)
+    # Convert to HSV color space
+    hsv_img = cv2.cvtColor(sample_img, cv2.COLOR_BGR2HSV)
+    # Blur image to reduce unrelated noise
+    blur_img = cv2.bilateralFilter(hsv_img, 9, 75, 75)
+    return blank_canvas, sample_img, rgb_img, hsv_img, blur_img
 
 
 def get_red_mask(img, lower_color1, upper_color1, lower_color2, upper_color2):
-	masked_img1 = cv2.inRange(img, lower_color1, upper_color1)
-	masked_img2 = cv2.inRange(img, lower_color2, upper_color2)
-	red_mask = masked_img1 + masked_img2
-	return red_mask
+    masked_img1 = cv2.inRange(img, lower_color1, upper_color1)
+    masked_img2 = cv2.inRange(img, lower_color2, upper_color2)
+    red_mask = masked_img1 + masked_img2
+    return red_mask
 
 
 def dilate_erode(img, kernel_size):
-	kernel = np.ones((kernel_size, kernel_size), np.uint8)
-	dilated_img = cv2.dilate(img, kernel, iterations=1)
-	cleaned_img = cv2.erode(dilated_img, kernel, iterations=1)
-	return cleaned_img
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    dilated_img = cv2.dilate(img, kernel, iterations=1)
+    cleaned_img = cv2.erode(dilated_img, kernel, iterations=1)
+    return cleaned_img
 
 
 def find_contour(img):
-	_, all_contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-	all_sorted_cnt = sorted(all_contours, key=cv2.contourArea)[-1:]
-	largest_cnt = all_sorted_cnt[0]
-	return largest_cnt, all_sorted_cnt
+    _, all_contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    all_sorted_cnt = sorted(all_contours, key=cv2.contourArea)[-1:]
+    largest_cnt = all_sorted_cnt[0]
+    return largest_cnt, all_sorted_cnt
 
 
 def draw_contour(img, canvas, contour_thickness):
-	largest_cnt, all_sorted_cnt = find_contour(img)
-	for i, largest_cnt in enumerate(all_sorted_cnt):
-		cv2.drawContours(canvas, largest_cnt, -1, [0, 255, 0], contour_thickness)
+    largest_cnt, all_sorted_cnt = find_contour(img)
+    for i, largest_cnt in enumerate(all_sorted_cnt):
+        cv2.drawContours(canvas, largest_cnt, -1, [0, 255, 0], contour_thickness)
 
 
 def draw_box(img, canvas, contour_thickness):
-	pos = []
-	_, all_sorted_cnt = find_contour(img)
-	for i, cnt in enumerate(all_sorted_cnt):
-		colour = (0, 255, 0)
-		x, y, w, h = cv2.boundingRect(cnt)
-		if abs(w - h) > 1:
-			cv2.rectangle(canvas, (x, y), (x + w, y + h), colour, contour_thickness)
-			pos.append([x, y, w, h])
-	return pos, canvas
+    pos = []
+    _, all_sorted_cnt = find_contour(img)
+    for i, cnt in enumerate(all_sorted_cnt):
+        colour = (0, 255, 0)
+        x, y, w, h = cv2.boundingRect(cnt)
+        if abs(w - h) > 1:
+            cv2.rectangle(canvas, (x, y), (x + w, y + h), colour, contour_thickness)
+            pos.append([x, y, w, h])
+    return pos, canvas
 
 
 def fix_convex_defect(img):
-	# Detect convexity defects base on the contour.
-	largest_cnt, _ = find_contour(img)
-	hulls = cv2.convexHull(largest_cnt, returnPoints=False)
-	convexity_defects = cv2.convexityDefects(largest_cnt, hulls)
-	for i in range(convexity_defects.shape[0]):
-		s_before, e_before, f_before, d_before = convexity_defects[i, 0]
-		start_before = tuple(largest_cnt[s_before][0])
-		end_before = tuple(largest_cnt[e_before][0])
-		far_before = tuple(largest_cnt[f_before][0])
-		cv2.line(img, start_before, end_before, [255, 0, 0], 1)
-	convex_fix_kernel = np.ones((3, 3), np.uint8)
-	convex_dilated_img = cv2.dilate(img, convex_fix_kernel, iterations=1)
-	convex_fixed_img = cv2.erode(convex_dilated_img, convex_fix_kernel, iterations=1)
-	return convex_fixed_img
+    # Detect convexity defects base on the contour.
+    largest_cnt, _ = find_contour(img)
+    hulls = cv2.convexHull(largest_cnt, returnPoints=False)
+    convexity_defects = cv2.convexityDefects(largest_cnt, hulls)
+    for i in range(convexity_defects.shape[0]):
+        s_before, e_before, f_before, d_before = convexity_defects[i, 0]
+        start_before = tuple(largest_cnt[s_before][0])
+        end_before = tuple(largest_cnt[e_before][0])
+        far_before = tuple(largest_cnt[f_before][0])
+        cv2.line(img, start_before, end_before, [255, 0, 0], 1)
+    convex_fix_kernel = np.ones((3, 3), np.uint8)
+    convex_dilated_img = cv2.dilate(img, convex_fix_kernel, iterations=1)
+    convex_fixed_img = cv2.erode(convex_dilated_img, convex_fix_kernel, iterations=1)
+    return convex_fixed_img
 
 
 def load_template(template_path):
-	template = cv2.imread(template_path)
-	blurred_template = cv2.bilateralFilter(template,9,75,75)
-	gray_scale_template = cv2.cvtColor(blurred_template, cv2.COLOR_BGR2GRAY)
-	ret, thresh2 = cv2.threshold(gray_scale_template, 127, 255, 0)
-	_, template_contours, _ = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-	template_cnt = template_contours[0]
-	return gray_scale_template, template_cnt
+    template = cv2.imread(template_path)
+    blurred_template = cv2.bilateralFilter(template,9,75,75)
+    gray_scale_template = cv2.cvtColor(blurred_template, cv2.COLOR_BGR2GRAY)
+    ret, thresh2 = cv2.threshold(gray_scale_template, 127, 255, 0)
+    _, template_contours, _ = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    template_cnt = template_contours[0]
+    return gray_scale_template, template_cnt
 
 
 def shape_compare(contours, template_cnt):
-	similarity_level = cv2.matchShapes(contours, template_cnt, 1, 0.0)
-	return similarity_level
+    similarity_level = cv2.matchShapes(contours, template_cnt, 1, 0.0)
+    return similarity_level
 
 
 def expend_border(original_img, region):
-	[img_h,img_w] = original_img.shape[:2]    
-	[x,y,w,h] = r
-	# Expending a border around target area
-	crop_x = [x-border, x+w+border]
-	crop_y = [y-border, y+h+border]
-	if crop_x[0]<0 or crop_x[1]>img_w:
-		crop_x = [x, x+w+50]
-	if crop_y[0]<0 or crop_y[1]>img_h:
-		crop_y = [y, y+h+50]
-	crop_w = crop_x[1]-crop_x[0]
-	crop_h = crop_y[1]-crop_y[0]
-	return crop_x[0], crop_y[0], crop_w, crop_h
+    [img_h,img_w] = original_img.shape[:2]
+    [x,y,w,h] = r
+    # Expending a border around target area
+    crop_x = [x-border, x+w+border]
+    crop_y = [y-border, y+h+border]
+    if crop_x[0]<0 or crop_x[1]>img_w:
+        crop_x = [x, x+w+50]
+    if crop_y[0]<0 or crop_y[1]>img_h:
+        crop_y = [y, y+h+50]
+    crop_w = crop_x[1]-crop_x[0]
+    crop_h = crop_y[1]-crop_y[0]
+    return crop_x[0], crop_y[0], crop_w, crop_h
 
 
 def crop_and_hist(original_img, region, border):
-	x,y,w,h = expend_border(original_img, region)
-	crop_mask = np.zeros((h,w,3), dtype=np.uint8)
-	target = original_img[y:y+h,x:x+w,:].copy()
-	#Do histigram equalization over interested region
-	H,S,V = cv2.split(cv2.cvtColor(target, cv2.COLOR_BGR2HSV))
-	eq_v = cv2.equalizeHist(V)
-	eq_target = cv2.cvtColor(cv2.merge([H, S, eq_v]), cv2.COLOR_HSV2RGB)
-	eq_img = original_img.copy()
-	eq_img[y:y+h,x:x+w,:] = eq_target
-	return target, eq_target
+    x,y,w,h = expend_border(original_img, region)
+    crop_mask = np.zeros((h,w,3), dtype=np.uint8)
+    target = original_img[y:y+h,x:x+w,:].copy()
+    # Do histigram equalization over interested region
+    H,S,V = cv2.split(cv2.cvtColor(target, cv2.COLOR_BGR2HSV))
+    eq_v = cv2.equalizeHist(V)
+    eq_target = cv2.cvtColor(cv2.merge([H, S, eq_v]), cv2.COLOR_HSV2RGB)
+    eq_img = original_img.copy()
+    eq_img[y:y+h,x:x+w,:] = eq_target
+    return target, eq_target
 
 
 def show_hist(roi_hist):
-	color = ['b','g','r']
-	for i, c in  enumerate(color):
-		hist_target = cv2.calcHist([roi_hist],[i],None,[256],[0,256])
-		plt.plot(hist_target,color = c)
-		plt.xlim([0,256])
-	plt.show()
+    color = ['b','g','r']
+    for i, c in  enumerate(color):
+        hist_target = cv2.calcHist([roi_hist],[i],None,[256],[0,256])
+        plt.plot(hist_target,color = c)
+        plt.xlim([0,256])
+    plt.show()
 
 
 #------------------------ Main ------------------------------------
@@ -185,14 +185,13 @@ print(similarity_level_fix)
 border = 5
 target_region_pos, box_on_img = draw_box(final_mask, stop_rgb_img, 3)
 for r in target_region_pos:
-	before_eq, after_eq = crop_and_hist(stop_rgb_img, r, border)
-	plt.imshow(before_eq)
-	plt.show()
-	#show_hist(before_eq)
-	#plt.show()
-	plt.imshow(after_eq)
-	plt.show()
-	#show_hist(after_eq)
-	#plt.show()
-)
+    before_eq, after_eq = crop_and_hist(stop_rgb_img, r, border)
+    plt.imshow(before_eq)
+    plt.show()
+    # show_hist(before_eq)
+    # #plt.show()
+    # plt.imshow(after_eq)
+    # plt.show()
+    # show_hist(after_eq)
+    # plt.show()
 
