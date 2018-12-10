49b21ae65d58170525ca3a070a9093038e7414b6
luchen <44048738+luchen29@users.noreply.github.com>
2018-12-10 05:12:27 -0700

    Add files via upload

--- TSR/preprocessing/traffic_sign_detc_simple.py
+++ TSR/preprocessing/traffic_sign_detc_simple.py
@@ -1,3 +1,338 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+
+def video_to_frames(vedio_path):
+    vidcap = cv2.VideoCapture(vedio_path)
+    success,image = vidcap.read()
+    print("image shape:",image.shape)
+    img_set = []
+    count = 0
+    while success:
+        # Save frame as JPEG file
+        cv2.imwrite("frame%d.jpg" % count, image)
+        # Get 1 frame each 1 second:
+        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))
+        success,image = vidcap.read()
+        img_set.append(image)
+        print('Read a new frame: ', success)
+        count += 1
+    print("length of img_set",len(img_set))
+    return img_set
+
+def frames_to_video(input_img_list,outputpath,fps):
+    image_array = []
+    for img in input_img_list:
+        h = img.shape[1]
+        w = img.shape[0]
+        size =  (h,w)
+        img = cv2.resize(img,size)
+        image_array.append(img)
+    fourcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
+    out = cv2.VideoWriter(outputpath,fourcc, fps, size)
+    for i in range(len(image_array)):
+        out.write(image_array[i])
+    out.release()
+
+# def load_prep_img(img_path):
+#     sample_img = cv2.imread(img_path)
+#     size = sample_img.shape
+#     blank_canvas_red = np.zeros(size, dtype=np.uint8)
+#     blank_canvas_yellow_orange = np.zeros(size, dtype=np.uint8)
+#     blank_canvas_white = np.zeros(size, dtype=np.uint8)
+#     # Convert to RGB color space
+#     rgb_img = cv2.cvtColor(sample_img, cv2.COLOR_BGR2RGB)
+#     # Convert to HSV color space
+#     hsv_img = cv2.cvtColor(sample_img, cv2.COLOR_BGR2HSV)
+#     # Blur image to reduce unrelated noise
+#     blur_img = cv2.bilateralFilter(hsv_img, 9, 75, 75)
+#     return blank_canvas_red, blank_canvas_yellow_orange, blank_canvas_white, sample_img, rgb_img, hsv_img, blur_img
+
+def load_prep_img(sample_img):
+    size = sample_img.shape
+    blank_canvas_red = np.zeros(size, dtype=np.uint8)
+    blank_canvas_yellow_orange = np.zeros(size, dtype=np.uint8)
+    blank_canvas_white = np.zeros(size, dtype=np.uint8)
+    # Convert to RGB color space
+    rgb_img = cv2.cvtColor(sample_img, cv2.COLOR_BGR2RGB)
+    # Convert to HSV color space
+    hsv_img = cv2.cvtColor(sample_img, cv2.COLOR_BGR2HSV)
+    # Blur image to reduce unrelated noise
+    blur_img = cv2.bilateralFilter(hsv_img, 9, 75, 75)
+    return blank_canvas_red, blank_canvas_yellow_orange, blank_canvas_white, sample_img, rgb_img, hsv_img, blur_img
+
+def get_red_mask(img, lower_color1, upper_color1, lower_color2, upper_color2):
+    masked_img1 = cv2.inRange(img, lower_color1, upper_color1)
+    masked_img2 = cv2.inRange(img, lower_color2, upper_color2)
+    loose_red_mask = masked_img1 + masked_img2
+    return loose_red_mask
+
+def get_other_colors_mask(img, lower_color, upper_color):
+    masked_img = cv2.inRange(img, lower_color, upper_color)
+    other_colors_mask = masked_img
+    return other_colors_mask
+
+def dilate_erode(img, kernel_size):
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    dilated_img = cv2.dilate(img, kernel, iterations=1)
+    cleaned_img = cv2.erode(dilated_img, kernel, iterations=1)
+    return cleaned_img
+
+
+# def find_contour(img):
+#     _, all_contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+#     if not all_contours:
+#         return None, None
+#     all_sorted_cnt = sorted(all_contours, key=cv2.contourArea)[-1:]
+#     if cv2.contourArea(all_sorted_cnt[0]) < w*h*0.0125 or cv2.contourArea(all_sorted_cnt[0]) > w*h*0.05:
+#         return None, None
+#     largest_cnt = all_sorted_cnt[0]
+#     return largest_cnt, all_sorted_cnt
+
+def find_contour(img):
+    _, all_contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    if len(all_contours) != 0:
+        all_sorted_cnt = sorted(all_contours, key=cv2.contourArea)[-1:]
+        largest_cnt = all_sorted_cnt[0]
+    else:
+        largest_cnt = None
+        all_sorted_cnt = None
+        print("no contours")
+    return largest_cnt, all_sorted_cnt
+
+
+def draw_contour_2(img, canvas, contour_thickness):
+    largest_cnt, all_sorted_cnt = find_contour(img)
+    if all_sorted_cnt is not None:
+        for i, largest_cnt in enumerate(all_sorted_cnt):
+            cv2.drawContours(canvas, largest_cnt, -1, [0, 255, 0], contour_thickness)
+
+
+def fix_convex_defect(img):
+    # Detect convexity defects base on the contour.
+    cnt_before_convex, _ = find_contour(img)
+    hull_before = cv2.convexHull(cnt_before_convex, returnPoints=False)
+    #print(type(hull_before))
+    defects_before = cv2.convexityDefects(cnt_before_convex, hull_before)
+    if defects_before is not None:
+        for i in range(defects_before.shape[0]):
+            s_before, e_before, f_before, d_before = defects_before[i, 0]
+            start_before = tuple(cnt_before_convex[s_before][0])
+            end_before = tuple(cnt_before_convex[e_before][0])
+            far_before = tuple(cnt_before_convex[f_before][0])
+            cv2.line(img, start_before, end_before, [255, 0, 0], 1)
+        convex_fix_kernel = np.ones((3, 3), np.uint8)
+        convex_dilated_img = cv2.dilate(img, convex_fix_kernel, iterations=1)
+        convex_fixed_img = cv2.erode(convex_dilated_img, convex_fix_kernel, iterations=1)
+    else:
+        convex_fixed_img = img
+    return convex_fixed_img
+
+
+def load_template(template_path):
+    template = cv2.imread(template_path)
+    blurred_template = cv2.bilateralFilter(template,9,75,75)
+    gray_scale_template = cv2.cvtColor(blurred_template, cv2.COLOR_BGR2GRAY)
+    ret, thresh2 = cv2.threshold(gray_scale_template, 127, 255, 0)
+    _, template_contours, _ = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    template_cnt = template_contours[0]
+    return gray_scale_template, template_cnt
+
+
+def shape_compare(contours, template_cnt):
+    similarity_level = cv2.matchShapes(contours, template_cnt, 1, 0.0)
+    return similarity_level
+
+
+def draw_box(img, canvas, contour_thickness):
+    pos = []
+    _, all_sorted_cnt = find_contour(img)
+    for i, cnt in enumerate(all_sorted_cnt):
+        colour = (0, 255, 0)
+        x, y, w, h = cv2.boundingRect(cnt)
+        #cv2.rectangle(canvas, (x, y), (x + w, y + h), colour, contour_thickness)
+        pos.append([x, y, w, h])
+    #print(pos)
+    return pos, canvas
+
+# def expend_border(original_img, region):
+#     [img_h,img_w] = original_img.shape[:2]
+#     [x,y,w,h] = region
+#     # Expending a square border around the target region:
+#     crop_x = [x-border, x+w+border]
+#     crop_y = [y-border, y+h+border]
+#     if crop_x[0]<0 or crop_x[1]>img_w:
+#         crop_x = [x, x+w+50]
+#     if crop_y[0]<0 or crop_y[1]>img_h:
+#         crop_y = [y, y+h+50]
+#     crop_w = crop_x[1]-crop_x[0]
+#     crop_h = crop_y[1]-crop_y[0]
+#     return crop_x[0], crop_y[0], crop_w, crop_h
+
+def crop_and_hist(original_img, target_region_pos, border):
+    [img_h,img_w] = original_img.shape[:2]
+    [x,y,w,h] = target_region_pos[0]
+    # Original position: (should not chage this!)
+    box_x = [x, x+w]
+    box_y = [y, y+h]
+    crop_x0 = box_x[0]-border
+    crop_y0 = box_y[0]-border
+    crop_x1 = box_x[1]+border
+    crop_y1 = box_y[1]+border
+    if crop_x0 < 0:
+        crop_x0 = 0
+    else:
+        crop_x0 = crop_x0
+    if crop_y0 < 0:
+        crop_y0 = 0
+    else:
+        crop_y0 = crop_y0
+    if crop_x1 > img_w:
+        crop_x1 = img_w
+    else:
+        crop_x1 = crop_x1
+    if crop_y1 > img_h:
+        crop_y1 = img_h
+    else:
+        crop_y1 = crop_y1
+    # New position:
+    x_exp = [crop_x0, crop_x1]
+    y_exp = [crop_y0, crop_y1]
+    w_exp = x_exp[1] - x_exp[0]
+    h_exp = y_exp[1] - y_exp[0]
+    if w_exp > h_exp:
+        diff = w_exp - h_exp
+        h_exp = w_exp
+        if y_exp[0] - round(diff/2) < 0:
+            y_exp_new0 = 0
+            remains_h = round(diff/2) - y_exp[0]
+            y_exp_new1 = y_exp[1] + round(diff/2) + remains_h
+        else:
+            y_exp_new0 = y_exp[0] - round(diff/2)
+            if y_exp[1] + round(diff/2) > img_h:
+                y_exp_new1 = img_h
+                remains_h = y_exp[1] + round(diff/2) - img_h
+                y_exp_new0 = y_exp[0] - round(diff/2) - remains_h
+            else:
+                y_exp_new1 = y_exp[1] + round(diff/2)
+        y_exp_new = [y_exp_new0, y_exp_new1]
+        x_exp_new = x_exp
+    else:
+        diff = h_exp - w_exp
+        w_exp = h_exp
+        if x_exp[0] - round(diff/2) < 0:
+            x_exp_new0 = 0
+            remains_w = round(diff/2) - x_exp[0]
+            x_exp_new1 = x_exp[1] + round(diff/2) + remains_w
+        else:
+            x_exp_new0 = x_exp[0] - round(diff/2)
+            if x_exp[1] + round(diff/2) > img_w:
+                x_exp_new1 = img_w
+                remains_w = x_exp[1] + round(diff/2) - img_w
+                x_exp_new0 = x_exp[0] - round(diff/2) - remains_w
+            else:
+                x_exp_new1 = x_exp[1] + round(diff/2)
+        y_exp_new = y_exp
+        x_exp_new = [x_exp_new0, x_exp_new1]
+    
+    target_region_img = original_img[y_exp_new[0]:y_exp_new[1], x_exp_new[0]:x_exp_new[1],:].copy()
+    print("target_region_img shape:",target_region_img.shape)
+    return target_region_img
+
+# def crop_and_hist(original_img, target_region_pos, border):
+#     [img_h,img_w] = original_img.shape[:2]
+#     [x,y,w,h] = target_region_pos[0]
+#     crop_x = [x-border, x+w+border]
+#     crop_y = [y-border, y+h+border]
+#     if crop_x[0]<0:
+#         crop_x = [0, x+w+border]
+#     else: crop_x = [x-border, x+w+border]
+#     if crop_x[1]>img_w:
+#         crop_x = [x-border, img_w]
+#     else: crop_x = [x-border, x+w+border]
+#     if crop_y[0]<0:
+#         crop_y = [0, y+h+border]
+#     else: crop_y = [y-border, y+h+border]
+#     if crop_y[1]>img_h:
+#         crop_y = [y-border, img_h]
+#     crop_w = crop_x[1]-crop_x[0]
+#     crop_h = crop_y[1]-crop_y[0]
+#     # Get a square base on the larger border
+#     if crop_w>crop_h:
+#         diff = crop_w - crop_h
+#         ori_crop_h = crop_h
+#         crop_h = crop_w
+#         crop_mask = np.zeros((crop_h,crop_w,3), dtype=np.uint8)
+#         target_region_img = original_img[crop_y[0]-round(diff/2):crop_y[0]+ori_crop_h+round(diff/2), crop_x[0]:crop_x[0]+crop_w,:].copy()
+#     else:
+#         diff = crop_h - crop_w
+#         ori_crop_w = crop_w
+#         crop_w = crop_h
+#         crop_mask = np.zeros((crop_h,crop_w,3), dtype=np.uint8)
+#         target_region_img = original_img[crop_y[0]:crop_y[0]+crop_h, crop_x[0]-round(diff/2):crop_x[0]+ori_crop_w+round(diff/2),:].copy()
+#    return target_region_img
+
+def crop_possible_signs(similarity_level_ori, similarity_level_fix, final_mask, rgb_img):
+    if similarity_level_ori < 0.8 or similarity_level_fix < 0.8:
+        target_region_pos, box_on_img = draw_box(final_mask, rgb_img, 1)
+        print(target_region_pos)
+        #         plt.imshow(box_on_img)
+        #         plt.show()
+        border = 8
+        target_region_img_before_eq = crop_and_hist(rgb_img, target_region_pos, border)
+        output = target_region_img_before_eq
+    else: return None
+    return output
+
+# def crop_possible_signs(final_mask, rgb_img):
+#     target_region_pos = draw_box(final_mask, rgb_img, 1)
+#     print("target_region_pos",target_region_pos)
+#     border = 8
+#     target_region_img = crop_and_hist(rgb_img, target_region_pos, border)
+#     output = target_region_img
+#     return output,target_region_pos
+
+def text_box_on_img(final_mask,img_draw_box,text):
+    #print("input to /text_box_on_img/ func:",type(img_draw_box))
+    target_region_pos, img_draw_box = draw_box_text(final_mask, img_draw_box, contour_thickness=2)
+    #plt.imshow(img_draw_box)
+    #plt.show
+    #print("output of func /draw_box_text/:",type(img_draw_box))
+    li = target_region_pos[0]
+    x = li[0]
+    y = li[1]
+    color = (0,255,0)
+    # img_draw_box is a drawed img (contains box, but no text)
+    img_draw_box = Image.fromarray(img_draw_box)
+    #print('type of img_draw_box after Image.fromarray:',type(img_draw_box))
+    draw = ImageDraw.Draw(img_draw_box)
+    draw.text((x, y), text, fill=color)
+    #plt.imshow(img_draw_box)
+    #plt.show
+    img_draw_box = np.asarray(img_draw_box)
+    #print("output text_box_on_img:",type(img_draw_box))
+    return img_draw_box
+
+# Load in the Octagon template and get its contours for red canvas
+octagon_template_loc = 'red_octagon.jpeg'
+gray_octagon_template, octagon_template_cnt = load_template(octagon_template_loc)
+# plt.imshow(gray_octagon_template,cmap='gray')
+# plt.show()
+
+# Load in the Rectagule template and get its contours for white canvas
+rectangle_template_loc = 'rectangle.png'
+gray_rectangle_template, rectangle_template_cnt = load_template(rectangle_template_loc)
+# plt.imshow(gray_rectangle_template,cmap='gray')
+# plt.show()
+
+# Load in the Dimond shape template and get its contours for red canvas
+dimond_shape_template_loc = 'dimond_shape.jpeg'
+gray_dimond_shape_template, dimond_shape_template_cnt = load_template(dimond_shape_template_loc)
+# plt.imshow(gray_dimond_shape_template,cmap='gray')
+# plt.show()
+
+
 # Read image(whatever color) as sample_img and display it.
 video_path = 'sample_images/video_test_8.mov'
 input_frames_set = video_to_frames(video_path)
